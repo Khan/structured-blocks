5b919661c47ef74cf3a066fc7c5d563e46be04da
John Resig <jeresig@gmail.com>
2014-11-19 13:26:43 -0500

    Add toolbox groups, making it possible to reorganize them. Fixes #30. Number scrubber and blocks no longer get clipped by container. Fixes #38.

--- js/structured-js.js
+++ js/structured-js.js
@@ -56,7 +56,9 @@ var JSToolbox = Backbone.View.extend({
         var toolbox = this.toolbox;
 
         _.keys(toolbox).forEach(function(category) {
-            html.push("<h3>" + category + "</h3>");
+            var groupHTML = [];
+
+            groupHTML.push("<h3>" + category + "</h3>");
 
             toolbox[category].forEach(function(item) {
                 var $item = item.render().$el;
@@ -64,6 +66,7 @@ var JSToolbox = Backbone.View.extend({
                 $item.data("drag-data", item.toAST());
 
                 $item.draggable({
+                    appendTo: ".block-toolbox-editor",
                     connectToSortable: ".block-statements",
                     helper: function() {
                         return $item.clone(true);
@@ -71,8 +74,10 @@ var JSToolbox = Backbone.View.extend({
                     revert: false
                 });
 
-                html.push($item);
+                groupHTML.push($item);
             });
+
+            html.push($("<div>").addClass("toolbox-group").html(groupHTML));
         });
 
         this.$el.html(html);
@@ -153,6 +158,7 @@ var JSToolboxEditor = Backbone.View.extend({
     },
 
     render: function() {
+        this.$el.addClass("block-toolbox-editor");
         this.$el.children().detach();
         this.$el.append([
             this.editor.render().$el,
@@ -579,6 +585,7 @@ var JSRule = Backbone.View.extend({
         var added = false;
 
         $div.sortable({
+            appendTo: ".block-toolbox-editor",
             revert: false,
             handle: ".block-wrapper > :first-child",
             helper: function(e, $item) {
